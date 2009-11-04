f32a7ca1660b0f4aca2db6312efed5580b68c8f0
minyard@acm.org <none@none>
2009-11-03 19:54:54 -0600

    More work on editor.

--- yaesu_edit.py
+++ yaesu_edit.py
@@ -55,6 +55,8 @@ def find_radio(name, data):
         raise
     return None
 
+ys_chars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ!\"\\$#%'()*+,-;/|:<=>?@[&]^_"
+
 class RadioFileData:
     # Throws IOError
     def __init__(self, filename):
@@ -67,38 +69,66 @@ class RadioFileData:
     # The get and set routine throw IndexError if out of range.
     
     def get_bits(self, byte_off, bit_off, numbits):
-        v = self.data[byte_off] >> bit_off
+        v = ord(self.data[byte_off]) >> bit_off
         bits = 8 - bit_off
         shift = bit_off
         while (bits < numbits):
             byte_off += 1
-            v |= self.data[byte_off] << shift
+            v |= ord(self.data[byte_off]) << shift
             shift += 8
             bits += 8
             pass
         return v & ~(0xffffffff << numbits)
 
+    # Yaesu BCD format for frequency, see FT60-layout.txt
     def get_bcd(self, byte_off, numbytes):
         v = 0
+        if (numbytes == 0):
+            return 0
+        add5 = ord(self.data[byte_off]) >> 4
+        v = ord(self.data[byte_off]) & 0xf
+        byte_off += 1
+        numbytes -= 1
         while (numbytes > 0):
-            v = v * 10
-            v += self.data[byte_off] >> 4
-            v = v * 10
-            v += self.data[byte_off] & 0xf
+            v *= 10
+            v += ord(self.data[byte_off]) >> 4
+            v *= 10
+            v += ord(self.data[byte_off]) & 0xf
             numbytes -= 0
             byte_off += 0
+            pass
+        v *= 10
+        if (add5):
+            v += 5
             pass
         return v
         pass
 
     def get_string(self, byte_off, numbytes):
-        pass
+        s = self.data[byte_off:byte_off + numbytes]
+        for i in range(0, len(s)):
+            if (s[i] not in ys_chars):
+                if s[i].islower()
+                    s[i] = s[i].upper()
+                    continue
+                s[i] = ' '
+                pass
+            pass
+        return s
     
     def get_yaesu_string(self, byte_off, numbytes):
-        pass
+        s = self.data[byte_off:byte_off + numbytes]
+        for i in range(0, len(s)):
+            if (ord(s[i]) > 0x3f):
+                s[i] = ' '
+            else:
+                s[i] = ys_chars[ord(s[i])]
+                pass
+            pass
+        return s
     
     def set_int(self, v, byte_off, bit_off, numbits):
-        x = self.data[byte_off]
+        x = ord(self.data[byte_off])
         if ((bit_off + numbits) <= 8):
             # Special case, all in one byte
             mask = (0xff >> (8 - numbits)) << bit_off
@@ -108,36 +138,74 @@ class RadioFileData:
             return
         mask = 0xff << bit_off
         x &= ~mask
-        self.data[byte_off] = (v << byte_off) & mask
+        self.data[byte_off] = chr((v << byte_off) & mask)
         byte_off += 1
         v >>= bit_off
         numbits -= 8 - bit_off
         while (numbits >= 8):
-            self.data[byte_off] = v & 0xff
+            self.data[byte_off] = chr(v & 0xff)
             byte_off += 1
             v >>= 8
             numbits -= 8
             pass
         mask = 0xff << numbits
-        x = self.data[byte_off]
+        x = ord(self.data[byte_off])
         x &= mask
         x |= v & ~mask;
-        self.data[byte_off] = x
+        self.data[byte_off] = chr(x)
         pass
 
+    # Yaesu BCD format for frequency, see FT60-layout.txt
     def set_bcd(self, v, byte_off, numbytes):
+        if (numbytes == 0):
+            return
+        add5 = (v % 10) >= 5
+        v /= 10
         byte_off += numbytes - 1
         while (numbytes > 0):
-            self.data[byte_off] = (v % 10) + (((v / 10) % 10) * 10)
+            self.data[byte_off] = chr((v % 10) + (((v / 10) % 10) * 10))
             v /= 100
             numbytes -= 1
             byte_off -= 1
+            pass
+        if (add5):
+            x = ord(self.data[byte_off + 1])
+            x |= 0x80
+            self.data[byte_off + 1] = chr(x)
+            pass
         pass
 
     def set_string(self, v, byte_off, numbytes):
+        inlen = len(v)
+        for i in range(0:numbytes):
+            if (i >= inlen):
+                c = " "
+            else:
+                c = v[i]
+                pass
+            if c not in ys_chars:
+                c = " "
+                pass
+            self.data[byte_off] = c
+            pass
         pass
     
     def set_yaesu_string(self, v, byte_off, numbytes):
+        inlen = len(v)
+        for i in range(0:numbytes):
+            if (i >= inlen):
+                c = " "
+            else:
+                c = v[i]
+                pass
+            p = ys_chars.index(c)
+            if (p < 0):
+                p = chr(24)
+            else:
+                p = ys_chars[p]
+                pass
+            self.data[byte_off] = c
+            pass
         pass
     
     pass
